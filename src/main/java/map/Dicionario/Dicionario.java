653f1feddfbf751485f1b773db54338452ab3c7f
Andreia-Nunes <andreia.nunes@academico.ifpb.edu.br>
2023-09-09 17:09:45 -0300

    Resolvendo desafio Dicionário

---
+++ src/main/java/map/Dicionario/Dicionario.java
@@ -0,0 +1,29 @@
+package main.java.map.Dicionario;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Dicionario {
+
+    private Map<String, String> dicionario;
+
+    public Dicionario() {
+        this.dicionario = new HashMap<>();
+    }
+
+    public void adicionarPalavra(String palavra, String definicao){
+        this.dicionario.put(palavra, definicao);
+    }
+
+    public void removerPalavra(String palavra){
+        this.dicionario.remove(palavra);
+    }
+
+    public void exibirPalavras(){
+        System.out.println(this.dicionario);
+    }
+
+    public String pesquisarPorPalavra(String palavra){
+        return this.dicionario.get(palavra);
+    }
+}
---
+++ src/main/java/map/Dicionario/Teste.java
@@ -0,0 +1,23 @@
+package main.java.map.Dicionario;
+
+public class Teste {
+    public static void main(String[] args) {
+        Dicionario dicionario = new Dicionario();
+
+        dicionario.adicionarPalavra("amor", "forte afeição por outra pessoa, nascida de laços de consanguinidade ou de relações sociais.");
+        dicionario.adicionarPalavra("amizade", "entimento de grande afeição, simpatia, apreço entre pessoas ou entidades.");
+        dicionario.adicionarPalavra("felicidade", "qualidade ou estado de feliz");
+        dicionario.adicionarPalavra("felicidade", "estado de uma consciência plenamente satisfeita");
+        dicionario.adicionarPalavra("tristeza", "estado afetivo caracterizado pela falta de alegria, pela melancolia");
+        dicionario.adicionarPalavra("felicidade", "satisfação, contentamento, bem-estar");
+
+        dicionario.exibirPalavras();
+
+        dicionario.removerPalavra("tristeza");
+
+        dicionario.exibirPalavras();
+
+        System.out.println(dicionario.pesquisarPorPalavra("felicidade"));
+
+    }
+}
